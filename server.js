e996dc296e41e817de80189e3be916f4dfef00e6
ernestocostanzo51 <ernestanzo51@gmail.com>
2026-04-20 17:23:43 +0200

    import sul server js del router

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const filmRouter = require('./routers/filmRouter')
+
+
+app.use(express.json());
+app.use('/movies', filmRouter);
 
 app.get('/' , (req , res) => {
     res.send('bevenuto in pagina')
