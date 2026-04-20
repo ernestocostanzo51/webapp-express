c076950933968cb464ede1f06ff4813954120150
ernestocostanzo51 <ernestanzo51@gmail.com>
2026-04-20 16:29:55 +0200

    aggiunto join e export index e show

--- controllers/filmController.js
+++ controllers/filmController.js
@@ -13,13 +13,24 @@ const show = (req , res) => {
     const { id } = req.params;
     const movieSql = 'SELECT * FROM movies WHERE id = ?'
 
-    connection.query(reviewsSql, [id], (err, reviewResults) => {
+   connection.query(movieSql, [id], (err, movieResults) => {
+        if (err) return res.status(500).json({ error: 'Errore del server' });
+        if (movieResults.length === 0) return res.status(404).json({ error: 'Film non trovato' });
+
+        const movie = movieResults[0];
+
+        
+        const reviewsSql = 'SELECT * FROM reviews WHERE movie_id = ?';
+        
+        connection.query(reviewsSql, [id], (err, reviewResults) => {
             if (err) return res.status(500).json({ error: 'Errore nel recupero recensioni' });
+            
             
             movie.reviews = reviewResults;
             res.json(movie);
         });
-
-    
+    });
 
 }
+
+module.exports = { index, show };
